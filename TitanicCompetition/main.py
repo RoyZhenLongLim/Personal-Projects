a8ab241832e02addddc9f8714868f1d99120a6be
Roy Lim <roy.lim@student.unsw.edu.au>
2022-01-17 15:42:08 +1100

    Removed print statement

--- TitanicCompetition/main.py
+++ TitanicCompetition/main.py
@@ -10,13 +10,10 @@ import pandas as pd # data processing, CSV file, I/O
 
 # ? train is the data of a subset of all passengers (more specifically 891, we
 # ? to predict the fates of the remaining 418 passengers on board).
-# train = pd.read_csv("Data/train.csv")
-train = pd.read_csv("TitanicCompetition/Data/train.csv")
-print(train.head)
+train = pd.read_csv("Data/train.csv")
+# train = pd.read_csv("TitanicCompetition/Data/train.csv")
 
 # ? test is the information of the remaining 418 passengers, there is no
 # ? survived column
-# test = pd.read_csv("Data/test.csv")
-test = pd.read_csv("TitanicCompetition/Data/test.csv")
-
-
+test = pd.read_csv("Data/test.csv")
+# test = pd.read_csv("TitanicCompetition/Data/test.csv")
